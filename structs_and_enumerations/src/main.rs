304c624593446a6632469ceba58e57a99102f471
Mortimer Pavlitski <mortimer.pavlitski@codethink.co.uk>
2024-08-11 23:06:25 +0100

    S2-P1 wip

--- structs_and_enumerations/src/main.rs
+++ structs_and_enumerations/src/main.rs
@@ -1,11 +1,12 @@
 use std::fs;
 use std::env::args;
 use std::error::Error;
+use std::collections::HashMap;
 
 #[derive(Debug)]
 enum LineData {
-    NameAndNumber{name: String, number: u32},
-    NameOnly{name: String}
+    NameAndNumber(String, u32),
+    NameOnly(String)
 }
 
 impl TryFrom<&str> for LineData {
@@ -15,16 +16,14 @@ impl TryFrom<&str> for LineData {
 
         if !line.contains(':')
         {
-            return Ok(LineData::NameOnly{name: line.to_string()});
+            return Ok(LineData::NameOnly(line.to_string()));
         } else {
             let parts: Vec<&str> = line.splitn(2, ":").collect();
 
-            let na: &str = parts[0];
-            let nu: u32 = parts[1].parse::<u32>()?;
-            return Ok(LineData::NameAndNumber{name: na.to_string(), number: nu})
+            let name: &str = parts[0];
+            let number: u32 = parts[1].parse::<u32>()?;
+            return Ok(LineData::NameAndNumber(name.to_string(), number))
         }
-        return Ok(LineData::NameOnly{name: "mortimer".to_string()});
-
     }
 }
 
@@ -48,11 +47,47 @@ fn main() -> Result<(), Box<dyn Error>>{
     let file_name = args().nth(1).ok_or("Expected filename")?;
 
     let file_data = parse_file(&file_name)?;
+
+    let mut score_cards:HashMap<String, ScoreCard>;
     
     for l in file_data {
+
+        match score_cards {
+            LineData::NameOnly(name) => score_cards.entry(name).or_insert(ScoreCard::default())
+        }
+
         println!("{:?}", l);
     }
     
     Ok(())
 }
 
+#[derive(Default)]
+struct ScoreCard {
+    running_total: u32,
+    tests_taken: u32,
+    missed_tests: u32,
+}
+
+impl ScoreCard {
+    fn add_score(&mut self, new_score: u32) {
+        self.running_total += new_score;
+        self.tests_taken += 1;
+    }
+
+    fn missed_test(&mut self) {
+        self.missed_tests += 1;
+    }
+
+    fn get_total(&self) -> u32 {
+        self.running_total
+    }
+
+    fn get_tests_taken(&self) -> u32 {
+        self.tests_taken
+    }
+
+    fn get_missed_tests(&self) -> u32 {
+        self.missed_tests
+    }
+}
