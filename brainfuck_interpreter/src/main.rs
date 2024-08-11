98b745cd59618ffbbf0be95284ab38e07d3af8f1
Mortimer Pavlitski <mortimer.pavlitski@codethink.co.uk>
2024-08-11 12:23:11 +0100

    Remove bookending ---8<---

--- brainfuck_interpreter/src/main.rs
+++ brainfuck_interpreter/src/main.rs
@@ -10,16 +10,22 @@ fn main() -> Result<(), Box<dyn Error>>{
 
     // Get file content as string.
     let file_contents = fs::read_to_string(&file_name)?; // Should be using `BufRead`
-    
 
-    // Valid command chars
-    let valid = ['>', '<', '+', '-', '.', ',', '[', ']'];
-
-    // Get only the valid chars
-    let parsed: Vec<char> = file_contents.to_string().chars().filter(|c| valid.contains(c)).collect();
+    // Remove whitespace
+    let parsed: Vec<char> = file_contents.to_string().chars().filter(|c| !c.is_whitespace()).collect();
     
+    // Remove bookends
+    let file_bookend: Vec<char>  = "---8<---".chars().collect();
+    let start = if parsed.starts_with(&file_bookend) {file_bookend.len()} else {0};
+    let end = if parsed.ends_with(&file_bookend) {file_bookend.len()} else {0};
+    let body: Vec<char> = parsed[start..parsed.len()-end].to_vec();
+    
+    // Remove invalid chars
+    let valid_chars = ['>', '<', '+', '-', '.', ',', '[', ']'];
+    let valid: Vec<char> = body.clone().into_iter().filter(|c| valid_chars.contains(c)).collect(); // Feel like I should be using borrow/mut to avoid the .clone().into_iter()
+
     // Format for printing
-    let output: String = parsed.iter().collect();
+    let output = valid.iter().collect::<String>();
 
     println!("{output}");
     
