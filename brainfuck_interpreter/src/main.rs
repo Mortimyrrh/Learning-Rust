e97eea065cc1a1257a3f73d8fbef765f0d14235a
Mortimer Pavlitski <mortimer.pavlitski@codethink.co.uk>
2024-08-19 18:46:56 +0100

    S2-P2 halfway commit

--- brainfuck_interpreter/src/main.rs
+++ brainfuck_interpreter/src/main.rs
@@ -1,35 +1,20 @@
+use core::fmt;
 use std::env::args;
 use std::error::Error;
+use std::fmt::write;
 use std::fs;
+use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // S1 P2 - In for the long haul
+    // S2 P2 - Another step toward an interpreter
 
-    // Get file name from args.
     let file_name = args().nth(1).ok_or("Expected filename")?;
 
-    // Get file content as string.
-    let file_contents = fs::read_to_string(&file_name)?; // Should be using `BufRead`
+    let instructions = parse_file(file_name.into())?;
 
-    // // Remove whitespace
-    // let parsed: Vec<char> = file_contents.to_string().chars().filter(|c| !c.is_whitespace()).collect();
-
-    // // Remove bookends
-    // let file_bookend: Vec<char>  = "---8<---".chars().collect();
-    // let start = if parsed.starts_with(&file_bookend) {file_bookend.len()} else {0};
-    // let end = if parsed.ends_with(&file_bookend) {file_bookend.len()} else {0};
-    // let body: Vec<char> = parsed[start..parsed.len()-end].to_vec();
-
-    // // Remove invalid chars
-    let valid_chars = ['>', '<', '+', '-', '.', ',', '[', ']'];
-    // let valid: Vec<char> = body.clone().into_iter().filter(|c| valid_chars.contains(c)).collect(); // Feel like I should be using borrow/mut to avoid the .clone().into_iter()
-
-    let parsed: Vec<char> = file_contents
-        .chars()
-        .filter(|c| valid_chars.contains(c))
-        .collect();
-
-    println!("{}", parsed.iter().collect::<String>());
+    for instruction in instructions {
+        println!("{}", instruction);
+    }
 
     Ok(())
 }
@@ -43,3 +28,89 @@ fn main() -> Result<(), Box<dyn Error>> {
 //     };
 // }
 // dbg!(parsed);
+
+enum RawInstruction {
+    MoveRight,
+    MoveLeft,
+    IncrementData,
+    DecrementData,
+    Output,
+    Input,
+    BeginLoop,
+    EndLoop,
+}
+
+impl RawInstruction {
+    fn from_char(c: char) -> Option<RawInstruction> {
+        match c {
+            '>' => Some(RawInstruction::MoveRight),
+            '<' => Some(RawInstruction::MoveLeft),
+            '+' => Some(RawInstruction::IncrementData),
+            '-' => Some(RawInstruction::DecrementData),
+            '.' => Some(RawInstruction::Output),
+            ',' => Some(RawInstruction::Input),
+            '[' => Some(RawInstruction::BeginLoop),
+            ']' => Some(RawInstruction::EndLoop),
+            _ => None,
+        }
+    }
+}
+
+impl fmt::Display for RawInstruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            RawInstruction::MoveRight => write!(f, "MoveRight"),
+            RawInstruction::MoveLeft => write!(f, "MoveLeft"),
+            RawInstruction::IncrementData => write!(f, "IncrementData"),
+            RawInstruction::DecrementData => write!(f, "DecrementData"),
+            RawInstruction::Output => write!(f, "Output"),
+            RawInstruction::Input => write!(f, "Input"),
+            RawInstruction::BeginLoop => write!(f, "BeginLoop"),
+            RawInstruction::EndLoop => write!(f, "EndLoop"),
+        }
+    }
+}
+
+struct InputInstruction {
+    raw_instruction: RawInstruction,
+    line_number: usize,
+    char_column: usize,
+}
+
+fn parse_file(path: PathBuf) -> Result<Vec<InputInstruction>, Box<dyn Error>> {
+    let file_contents = fs::read_to_string(&path)?;
+
+    let mut instructions: Vec<InputInstruction> = Vec::new();
+
+    let mut line_number = 0;
+    let mut char_column = 0;
+
+    for line in file_contents.lines() {
+        for character in line.chars() {
+            match RawInstruction::from_char(character) {
+                Some(raw_instruction) => instructions.push(InputInstruction {
+                    raw_instruction: raw_instruction,
+                    line_number: line_number,
+                    char_column: char_column,
+                }),
+                None => (), // ignore invalid characters for now,
+            }
+            char_column += 1;
+        }
+        line_number += 1;
+    }
+
+    Ok(instructions)
+}
+
+impl fmt::Display for InputInstruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{} at line:{}, col:{}",
+            self.raw_instruction, self.line_number, self.char_column
+        )
+    }
+}
+
+
