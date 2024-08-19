bea62943f3dfeeb04d583a259cbe9e1de6f78326
Mortimer Pavlitski <mortimer.pavlitski@codethink.co.uk>
2024-08-19 17:08:05 +0100

    S1-P2 tidy up

--- brainfuck_interpreter/src/main.rs
+++ brainfuck_interpreter/src/main.rs
@@ -1,34 +1,36 @@
-use std::fs;
 use std::env::args;
 use std::error::Error;
+use std::fs;
 
-fn main() -> Result<(), Box<dyn Error>>{
+fn main() -> Result<(), Box<dyn Error>> {
     // S1 P2 - In for the long haul
-    
+
     // Get file name from args.
     let file_name = args().nth(1).ok_or("Expected filename")?;
 
     // Get file content as string.
     let file_contents = fs::read_to_string(&file_name)?; // Should be using `BufRead`
 
-    // Remove whitespace
-    let parsed: Vec<char> = file_contents.to_string().chars().filter(|c| !c.is_whitespace()).collect();
-    
-    // Remove bookends
-    let file_bookend: Vec<char>  = "---8<---".chars().collect();
-    let start = if parsed.starts_with(&file_bookend) {file_bookend.len()} else {0};
-    let end = if parsed.ends_with(&file_bookend) {file_bookend.len()} else {0};
-    let body: Vec<char> = parsed[start..parsed.len()-end].to_vec();
-    
-    // Remove invalid chars
+    // // Remove whitespace
+    // let parsed: Vec<char> = file_contents.to_string().chars().filter(|c| !c.is_whitespace()).collect();
+
+    // // Remove bookends
+    // let file_bookend: Vec<char>  = "---8<---".chars().collect();
+    // let start = if parsed.starts_with(&file_bookend) {file_bookend.len()} else {0};
+    // let end = if parsed.ends_with(&file_bookend) {file_bookend.len()} else {0};
+    // let body: Vec<char> = parsed[start..parsed.len()-end].to_vec();
+
+    // // Remove invalid chars
     let valid_chars = ['>', '<', '+', '-', '.', ',', '[', ']'];
-    let valid: Vec<char> = body.clone().into_iter().filter(|c| valid_chars.contains(c)).collect(); // Feel like I should be using borrow/mut to avoid the .clone().into_iter()
+    // let valid: Vec<char> = body.clone().into_iter().filter(|c| valid_chars.contains(c)).collect(); // Feel like I should be using borrow/mut to avoid the .clone().into_iter()
 
-    // Format for printing
-    let output = valid.iter().collect::<String>();
+    let parsed: Vec<char> = file_contents
+        .chars()
+        .filter(|c| valid_chars.contains(c))
+        .collect();
 
-    println!("{output}");
-    
+    println!("{}", parsed.iter().collect::<String>());
+
     Ok(())
 }
 
